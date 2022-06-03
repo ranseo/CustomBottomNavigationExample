6b7becdb1242a7d5bdae77b52cbb44a81b01fe7e
ranseo <55490534+ranseo@users.noreply.github.com>
2022-06-03 14:14:15 +0900

    Initial commit

---
+++ app/src/main/java/com/example/custombottomnavigationexample/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.custombottomnavigationexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.custombottomnavigationexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
